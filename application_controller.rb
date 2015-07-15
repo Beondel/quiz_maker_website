fd68327ffde68030f62812e7c71178ae833029aa
igerstein <isaac.gerstein1@gmail.com>
2015-07-15 23:27:18 +0000

    Created basic page structure

--- application_controller.rb
+++ application_controller.rb
@@ -4,10 +4,21 @@ Bundler.require
 
 
 class MyApp < Sinatra::Base
-
-  get '/' do 
+  
+  get '/' do
     erb :index
   end
   
+  post '/quiz_list' do
+    erb :quiz_list
+  end
+  
+  post '/create_quiz' do
+    erb :create_quiz
+  end
+  
+  post '/quiz' do
+    erb :quiz
+  end
   
 end
