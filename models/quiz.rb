0ed8d32f023795c64142f89b045ab34b675a2ab3
Beondel <bgfmac@gmail.com>
2015-07-15 18:43:23 +0000

    updated Quiz class

--- models/quiz.rb
+++ models/quiz.rb
@@ -1,33 +1,36 @@
 class Quiz
-  def initialize(name, description, type = list)  
+  def initialize(name, description, answers, type = list)  
     @name = name
-    @answers = []
+    @answers = answers
+    @answers_list = []
     @completed = []
     @description = description
     @type = type
     @finished = false
   end
-  attr_accessor :name, :answers, :description, :type, :finished, :completed
+  attr_accessor :name, :answers, :description, :type, :finished, :completed, :answers_list
+ 
+#  def add_answer(answer)
+#    @answers << answer
+#  end
   
-  def add_answer(answer)
-    @answers << answer
+  def creat_answers_list
+    @answers_list = @answer.upcase.split(", ")
   end
   
   def is_complete
-    if @completed == @answers
+    if @completed == @answers_list
       @finished = true
       return "YOU WIN!!!"
     end
   end
   
   def check_answer(guess)
-    if @answers.include?(guess)
-      @completed << guess
+    if @answers_list.include?(guess.upcase)
+      @completed << guess.upcase
     end
   end
-  
 end
 
-usa = Quiz.new('USA STATES', 'list all of the states')
-usa.add_answer('new york')
+usa = Quiz.new('USA STATES', 'list all of the states', 'new york, new jersey, massachusetts, beau heffron')
 
